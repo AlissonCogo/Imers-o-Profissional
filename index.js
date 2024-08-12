8b6670018950dee1e98fc12f0945395d81d46f3f
Alisson <alisson.buffetti@gmail.com>
2024-08-12 20:42:31 -0300

    Get - Post - Delete com Express

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Vai Tomar no Cú Paulo')
-});
+app.use(express.json());
+
+const peaple = [
+    { id: 1, nome: 'João'},
+    { id: 2, nome: 'Maria'},
+    { id: 3, nome: 'José'},
+];
+
+app.get('/pessoa', function(req, res) {
+    //Retornar lista de usuario
+    res.json(peaple);
+
+})
+
+// Post /pessoa
+// {"id": "1", "name": "Rafael"}
+app.post('/pessoa', function(req, res){
+    //Inserir um usuário
+    const body = req.body;
+
+    const newPeaple = req.body;
+
+    peaple.push(newPeaple);
+
+    res.status(201).json(newPeaple);
+
+})
+
+app.delete('/pessoa/:id', function(req, res){
+    //Remover uma aplicação
+    const id = req.params.id;
+    const index = peaple.findIndex(p => p.id === id);
+
+    if (index !== -1) {
+        peaple.splice(index, 1);
+        res.status(204).send(); // 204 No Content
+    } else {
+        res.status(404).json({ message: 'Usuário não encontrado' });
+    }
+})
+
 
 app.listen(3000);
-
-app.post("/Pessoa/Cadastrar", function(req, res) {
-    res.json({
-        status: "OK",
-        message: "Pessoa Cadastrada"
-    }
-    )
-})
